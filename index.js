47985b899f1c8a1ade5902b9fe360c31a88ff6ff
Akash Chhetri <akash.chhetri1@gmail.com>
2020-08-31 14:42:39 +1000

    fixed project structures

---
+++ index.js
@@ -0,0 +1,6 @@
+const getDomain = require('./utility/getDomain');
+const randomStringGenerator = require("./utility/randomStringGenerator");
+
+
+module.exports.randomStringGenerator = randomStringGenerator;
+module.exports.getDomain = getDomain; 
